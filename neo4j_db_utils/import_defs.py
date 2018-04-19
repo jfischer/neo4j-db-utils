0cf5c0f364516c2d47f040f9af99e1ac6993f833
Jeff Fischer <jeff@data-ken.org>
2018-04-19 16:23:14 -0700

    2 to 3 compatibility examples

--- neo4j_db_utils/import_defs.py
+++ neo4j_db_utils/import_defs.py
@@ -1,41 +1,20 @@
 """
 Definitions for map-reduce based creation of Neo4j import files.
 
-We use Python's new typing to define the expected values (mostly just
-for documentation purposes). Type names that we define end in a captial "T".
-
-Enumerations are defined using the Enum functionity added in Python 3.4.
-It is pretty similar to defining constants under a namespace, but the type
-of the constants is the type of the Enum.
-
-After all the type definitions, we will define the class MapReduceTemplate.
-This is the class a user of the map-reduce infrastructure should subclass.
-
-The basic flow of the map-reduce infrastructure will be:
-
-1. Parse the headers.json input file to produce a list of dicts.
-2. Pass these dicts to map_input() to create a list of nodes and a list
-   of relationships.
-3. Each node and relationship is hashed to a map worker which writes them
-   to a file (using pickle?).
-4. When the entire input has been processed, reduce works are started
-5. Each reduce worker reads a subset of the node and relationship files
-   into memory.
-6. reduce_nodes() and reduce_relationships() are called to merge duplicate
-   entires for the same node or relastionship.
-7. node_to_csv_row() and rel_to_csv_row() are called by the reduce workers to
-   create CSV rows from the merged nodes/relationships. These are written
-   out to CSV files.
 """
-from abc import ABC, abstractmethod
+from abc import ABCMeta, abstractmethod
 from collections import namedtuple
 import string
+import sys
+from six import add_metaclass
+PYTHON_BASE_VERSION=sys.version_info[0]
 
 ##########################################################################
 #    Types for the Graph Components (nodes and relationships)
 ##########################################################################
 
-class Node(ABC):
+@add_metaclass(ABCMeta)
+class Node(object):
     """Subclass from this to represent your nodes.
     """
     @abstractmethod
@@ -66,7 +45,8 @@ class Node(ABC):
 RelId = namedtuple('RelId',
                    ['source_type', 'source_id', 'rel_type', 'dest_type', 'dest_id'])
 
-class Relationship(ABC):
+@add_metaclass(ABCMeta)
+class Relationship(object):
     """Subclass for your relationships. You will want to use __slots__
     when possible to reduce the memory usage of each instance. If you
     do not have any properties on the relationship, just use the
@@ -117,7 +97,8 @@ class SimpleRelationship(Relationship, RelId):
                 ':TYPE']
 
 
-class MapReduceTemplate(ABC):
+@add_metaclass(ABCMeta)
+class MapReduceTemplate(object):
     """This abstract class contains methods which provide the necessary
     implementation needed for the map-reduce algorithm.
     """
@@ -146,19 +127,30 @@ class MapReduceTemplate(ABC):
 #    General utilities for data cleansing
 ##########################################################################
 NONPRINTABLE=set('\n\r\x0b\x0c').union(set([chr(i) for i in range(128)]).difference(string.printable))
-XLATE={ord(character):chr(183) for character in NONPRINTABLE}
-# tab  converted to space
-XLATE[ord('\t')] = ' '
-def cleanup_text(text):
-    """Remove problematic characters for the CSV import"""
-    return text.translate(XLATE) if text is not None else None
+if PYTHON_BASE_VERSION==3:
+    XLATE={ord(character):chr(183) for character in NONPRINTABLE}
+    # tab  converted to space
+    XLATE[ord('\t')] = ' '
+    def cleanup_text(text):
+        """Remove problematic characters for the CSV import"""
+        return text.translate(XLATE) if text is not None else None
 
-XLATE_IDS={ord(character):chr(183) for character in NONPRINTABLE}
-XLATE_IDS[ord(' ')]=''
-XLATE_IDS[ord('\t')]=''
+    XLATE_IDS={ord(character):chr(183) for character in NONPRINTABLE}
+    XLATE_IDS[ord(' ')]=''
+    XLATE_IDS[ord('\t')]=''
 
-def cleanup_id(text):
-    """Remove problematic characters for the CSV import. Neo4j seems to ignore spaces in ids,
-    so we remove them completely."""
-    return text.translate(XLATE_IDS) if text is not None else None
+    def cleanup_id(text):
+        """Remove problematic characters for the CSV import. Neo4j seems to ignore spaces in ids,
+        so we remove them completely."""
+        return text.translate(XLATE_IDS) if text is not None else None
+else: # The translate() method behaves a little differently in python 2
+    DELETECHARS=''.join(NONPRINTABLE)+'\t'
+    def cleanup_text(text):
+        """Remove problematic characters for the CSV import"""
+        return text.translate(None, DELETECHARS) if text is not None else None
 
+    IDDELETECHARS=DELETECHARS+' '
+    def cleanup_id(text):
+        """Remove problematic characters for the CSV import. Neo4j seems to ignore spaces in ids,
+        so we remove them completely."""
+        return text.translate(NONE, IDDELETECHARS) if text is not None else None
